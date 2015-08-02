54b0258fcc9af1658ab1ce529f3a6ba864fcbeca
JoJoMimosa (Joachim) <joachimrochat@gmail.com>
2015-08-02 18:51:30 +0200

    First changes

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Le Nouveau Minecraft - News sur le jeu de type "sandbox"</title>
+		<link rel="stylesheet" href="style.css">
+		<link href='http://fonts.googleapis.com/css?family=Merriweather:400,700' rel='stylesheet' type='text/css'>
+
+		<!-- Métadonnées pour SEO -->
+		
+		<!-- Fin  meta pour SEO -->
+
+		<script type="text/javascript">
+		function init() {
+			window.addEventListener('scroll', function(){
+				if(document.body.scrollTop > 0) {
+					document.querySelector('header').style.background = "grey";
+				} else {
+					document.querySelector('header').style.background = "#25885B";
+				}
+			});
+		}
+		window.onload = init();
+		</script>
+	</head>
+	<body>
+		<header>
+			<h1>
+				<a href="/">Le Nouveau Minecraft</a>
+			</h1>
+		</header>
+		<article>
+			<h2>Des péniches dans l'espace</h2>
+			<div><img src="https://upload.wikimedia.org/wikipedia/pt/7/71/Minecraft2_logo.png"></div>
+			<p>"On l'a fait", se réjouissait ce matin le président Obama, en observant le convoi de péniches décoller. Sa réaction a conduit à l'indignation du côté républicain, qui récalme désormais des excuses pour "propos racistes" et "calomnies". En effet, les péniches étaient noires et d'une taille...</p>
+		</article>
+		<article>
+			<h2>Des péniches dans l'espace</h2>
+			<div><img src="https://upload.wikimedia.org/wikipedia/pt/7/71/Minecraft2_logo.png"></div>
+			<p>"On l'a fait", se réjouissait ce matin le président Obama, en observant le convoi de péniches décoller. Sa réaction a conduit à l'indignation du côté républicain, qui récalme désormais des excuses pour propos racistes et calomnies. En effet, les péniches étaient noires, et d'une taille...</p>
+		</article>
+		<article>
+			<h2>Des péniches dans l'espace</h2>
+			<div><img src="https://upload.wikimedia.org/wikipedia/pt/7/71/Minecraft2_logo.png"></div>
+			<p>"On l'a fait", se réjouissait ce matin le président Obama, en observant le convoi de péniches décoller. Sa réaction a conduit à l'indignation du côté républicain, qui récalme désormais des excuses pour "propos racistes" et "calomnies". En effet, les péniches étaient noires et d'une taille...</p>
+		</article>
+	</body>
+</html>
