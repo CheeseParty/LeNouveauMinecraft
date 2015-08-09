c9a1ce80fdbbbe2eb04a89fc47ee9846d7b45440
Gweedzy <pj.leborgne@gmail.com>
2015-08-09 23:12:14 +0200

    Fix Pass

--- yournewpassword.php
+++ yournewpassword.php
@@ -1,21 +1,23 @@
 <?php                       
 // On passe au changement (update bdd)
-// Si les 2 nouveaux pass entrés correspondent
+
+// On inclut ce qui est nécessaire à la connexion à la bdd
 require('includes/connexion.php');
 
-
-
+// Si les 2 nouveaux pass entrés correspondent
 if(isset($_POST['newpass']) && isset($_POST['newpass2']) && ($_POST['newpass'] === $_POST['newpass2'])) {
-    
-    // Alors on change le mot de passe
     $token = $_GET['token'];
+    // Alors on change le mot de passe
     $changemdp = $db -> prepare('UPDATE membres SET hash=:hash WHERE changepasskey=:token');
     $changemdp -> execute(array(
         'hash' => password_hash($_POST['newpass'], CRYPT_BLOWFISH),
         'token' => $token
     ));
     $changemdp -> closeCursor();
+    // Et on redirige
+    header('Location : changer_mdp.php?passwordchanged=true');
 } else {
+    // Sinon on redirige avec message d'erreur
     header('Location: changingpassword.php?error=true');
 }
 
