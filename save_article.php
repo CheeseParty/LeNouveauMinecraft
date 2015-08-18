85d9a79d52d3cd6c320dd6769771517fbc2e1bda
JoJoMimosa <joachimrochat@gmail.com>
2015-08-18 16:45:30 +0200

    WO BIST DU SCHATZ

--- save_article.php
+++ save_article.php
@@ -52,7 +52,59 @@ if(isset($_POST['mode']) AND $_POST['mode'] == "save") {
 
 # Si la requête ne vient pas en XHR (Sauver et publier)
 elseif(isset($_POST['mode']) AND $_POST['mode'] == "publish") {
-	echo "BANANA";
+	# Vérification de la requête
+	if(!isset($_POST['titre']) OR !isset($_POST['contenu'])) {
+		exit;
+	}
+
+	# Préparation de l'objet PDO
+	require('includes/connexion.php');
+    
+    # Si un id est donné, on update la colonne "publie"
+    if(isset($_POST['id'])) {
+        $update = $db -> prepare('UPDATE articles SET publication=:publication, publie=1 WHERE id=:id AND auteur=:auteur');
+        $update -> execute(array(
+            'publication' => ,
+            'id' => $_POST['id'],
+            'auteur' => $_SESSION['AUTH']
+        ));
+        
+        # Si une ligne a été affectée
+        if($db -> rowCount() > 0) {
+            header("Location: write.php");
+        }
+        
+        # Sinon
+        else {
+            exit;
+        }
+        
+        # On ferme la requête
+        $update -> closeCursor();
+    }
+    
+    # Sinon, on insert
+    else {
+        # Si on a le rang suffisant
+        if($_SESSION['RANK'] > 2) {
+            $insert = $db -> prepare('INSERT INTO articles SET (id, categorie, titre, contenu, publication, auteur, rang, publie, thumbnail) VALUES (:id, :categorie, :titre, :contenu, :publication, :auteur, :rang, :publie, :thumbnail)');
+            $insert -> execute(array(
+                'id' => '',
+                'categorie' => $_POST[''],
+                'titre' => $_POST['titre'],
+                'contenu' => $_POST['contenu'],
+                'publication' => date('Y-m-d G:i:s'),
+                'auteur' => $_SESSION['AUTH'],
+                'rang' => $_SESSION['AUTH'],
+                'publie' => 1,
+                'thumbnail' => $_POST['thumbnail']
+            ));
+            $insert -> closeCursor();
+            header("Location: write.php");
+        } else {
+            exit;
+        }
+    }
 }
 
 # Si la requête est invalide
