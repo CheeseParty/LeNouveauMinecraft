6941cc3495c42ad2d1e1fc803caafaf91a486247
Gweedzy <pj.leborgne@gmail.com>
2015-08-18 19:22:27 +0200

    Commentaires

---
+++ espace_commentaires.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Poster un commentaire - Nether News</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="commentaires.css">
+    </head>
+    
+    <body>
+        <header>
+            <a class="sitename" href="espace_commentaires.php">Nether News</a>
+            
+            <ul>
+                <li><a id="firstlink" class="currnavlink" href="#">Accueil</a></li>
+                <li><a class="navlink" href="#">Articles</a></li>
+                <li><a class="navlink" href="#">Informations</a></li>
+            </ul>
+            
+            <a href="#" class="loginlink">connexion</a>
+            <a href="#" class="registerlink">inscription</a>
+        </header>
+        
+        <div id="pagecontent">
+            
+        </div>
+    </body>
+</html>
