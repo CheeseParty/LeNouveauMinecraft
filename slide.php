8f3d3bc6bdfe0288f0c4dc441138b43bca60e510
Gweedzy <pj.leborgne@gmail.com>
2015-08-20 15:37:19 +0200

    .

---
+++ slide.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Nether News - Articles Minecraft : Maps, Mods, Plugins, Texture Pack...</title>
+        <meta charset="utf-8">
+        <link href='http://fonts.googleapis.com/css?family=Exo' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="slide.css">
+        <!-- Favicons -->
+        <?php require('includes/favicons.php'); ?>
+    </head>
+    
+    <body>
+        <header>
+            <img class="logo" src="img/logo.png" alt='logo' href="slide.php">
+            <ul id="menubar">
+                <li><a class="navlink" href="#">Accueil</a></li>
+                <li><a class="navlink" href="#">Divers</a></li>
+                <li><a class="navlink" href="#">Serveur</a></li>
+                <li><a class="navlink" href="#">Forum</a></li>
+            </ul>
+            <a class="registerlink" href="#" target="_blank">Inscription</a>
+            <a class="loginlink" href="#" target="_blank">Connexion</a>
+            
+        </header>
+        
+        <div id="topzone">
+            <div class="arrows">
+                <span><img src="arrow-right.svg" class="arrow-right" alt="fleche-droite">Lisez les articles qui vous intéressent</span>
+                <span><img src="arrow-right.svg" class="arrow-right" alt="fleche-droite">Découvrez</span>
+                <span><img src="arrow-right.svg" class="arrow-right" alt="fleche-droite">Téléchargez si ça vous plaît</span>
+            </div>
+            
+            <p class="toptext">Venez aussi jouez sur notre serveur ! Administrateurs à l'écoute des joueurs. Fluidité, bonne ambiance.</p>
+        </div>
+        
+        <div id="content">
+            <
+        </div>
+    </body>
+</html>
