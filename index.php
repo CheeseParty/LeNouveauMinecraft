b60f6d8796596521bf4015a2c6b1e1ca8686a68e
JoJoMimosa <joachimrochat@gmail.com>
2015-08-23 15:51:18 +0200

    awd

--- index.php
+++ index.php
@@ -49,26 +49,22 @@ if(!isset($_SESSION['AUTH'])) {
         <header id="header">
             <div id="header-container">
                 <div id="header-inner">
-                    <div id="hamburger">
+                    <div onclick="toggleMenu()" id="hamburger">
                         <span></span>
                         <span></span>
                         <span></span>
                     </div>
                     <a id="title" href="index.php"><h1>Nether News</h1></a>       
                     <?php if(isset($_SESSION['AUTH'])): ?>
-                        <a id="settings-btn" class='login'><img src='settings.svg'><span><?=$_SESSION['AUTH']?></span></a>
+                        <a class="login" onclick="toggleProfile()"><img src="login.svg"><span><?=$_SESSION['AUTH']?></span></a>
                     <?php else: ?>
-                        <a class='login' href='login.php'><img src='login.svg'><span>Connexion | Inscription</span></a>
+                        <a class="login" href="login.php"><img src="login.svg"><span>Connexion | Inscription</span></a>
                     <?php endif ?>
                     </a>
-                <!--<?php if(isset($_SESSION['AUTH'])): ?>
-                        <p class='infoauth'><span>Connecté en tant que <a href="user/<?=$_SESSION['AUTH']?>/"><?=$_SESSION['AUTH']?></a></span></p>
-                    <?php endif ?>-->
                 </div>
             </div>
         </header>
         <div id="menu" class="dropdown">
-            <a></a>
             <a href="news/">News</a>
             <a href="maps/">Maps</a>
             <a href="mods/">Mods</a>
@@ -80,10 +76,9 @@ if(!isset($_SESSION['AUTH'])) {
         </div>
         
         <?php if(isset($_SESSION['AUTH'])): ?>
-            <div id="settings" class="dropdown">
-                <a></a>
+            <div id="profile" class="dropdown">
                 <a href="user/<?=$_SESSION['AUTH']?>/">
-                    <img src="http://www.gravatar.com/avatar/<?=$_SESSION['MD5']?>?s=160">
+                    <img src="http://www.gravatar.com/avatar/<?=$_SESSION['MD5']?>?s=250">
                     Mon compte
                 </a>
                 <?php if($_SESSION['RANK'] > 1): ?>
@@ -141,26 +136,9 @@ if(!isset($_SESSION['AUTH'])) {
             <div>N</div>
             © "Copyright" Nether News <?=date('Y')?>
         </footer>
-        <?php if(isset($_SESSION['RANK']) AND $_SESSION['RANK'] > 1): ?>
-            <style>
-                #settings {
-                    top: -280px;
-                }
-
-                #settings a:nth-child(3) {
-                    background: #008CFF;
-                }
-            </style>
-        <?php endif ?>
         <script type="text/javascript">
-            <?php if(isset($_SESSION['AUTH']) AND $_SESSION['RANK'] > 1): ?>
-                var offset = "-280px";
+            <?php if(isset($_SESSION['AUTH'])): ?>
                 var logged = true;
-                var rotate = false;
-            <?php elseif(isset($_SESSION['AUTH'])): ?>
-                var logged = true;
-                var rotate = false;
-                var offset = "-240px";
             <?php else: ?>
                 var logged = false;
             <?php endif ?>
@@ -168,102 +146,39 @@ if(!isset($_SESSION['AUTH'])) {
 
 
         <!-- A FAIRE: METTRE CE SCRIPT DANS UN FICHIER EXTERNE -->
-        <script type="text/javascript" async defer>
-        // Anime les thumbnails des articles
-        function animateBg(a) {
-            a.style.backgroundSize = "120%";
-        }
-            
-        // Place le menu au bon endroit
-        function moveMenu() {
-            var margin = parseFloat(window.getComputedStyle(document.getElementById("header-container")).marginLeft);
-            var padding = parseFloat(window.getComputedStyle(document.getElementById("header-inner")).paddingLeft);
-            document.getElementById("menu").style.left = margin+padding+"px";
-        }
-
-        // Place les paramètres au bon endroit
-        function moveSettings() {
-            var margin = parseFloat(window.getComputedStyle(document.getElementById("header-container")).marginLeft);
-            var padding = parseFloat(window.getComputedStyle(document.getElementById("header-inner")).paddingLeft);
-            document.getElementById("settings").style.right = margin+padding+"px";
-        }
-
+        <script type="text/javascript" async defer>            
         // Toggle le menu
-        function initMenu() {
-            var burger = document.getElementById("hamburger");
-            var menu = document.getElementById("menu");
-            document.getElementById("hamburger").addEventListener('click', function() {
-                if(window.getComputedStyle(menu).top == "-350px") {
-                    menu.style.top = "55px";
-                    burger.style.transform = "rotate(90deg)";
-                    document.getElementById("articles").style.opacity = 0.5;
-                } else {
-                    menu.style.top = "-350px";
-                    burger.style.transform = "rotate(0)";
-                    document.getElementById("articles").style.opacity = 1;
-                }
-            });
-        }
-
-        // Toggle les settings
-        function initSettings() {
-            var settings = document.getElementById("settings");
-            var cog = document.querySelector("#settings-btn img");
-            document.getElementById("settings-btn").addEventListener('click', function() {
-                if(window.getComputedStyle(settings).top == offset) {
-                    settings.style.top = "55px";
-                    if(rotate) {
-                        cog.style.transform = "rotate(45deg)";
-                        console.log("90");
-                    }
-                    document.getElementById("articles").style.opacity = 0.5;
-                } else {
-                    settings.style.top = offset;
-                    if(rotate) {
-                        cog.style.transform = "rotate(0)";
-                        console.log("0");
-                    }
-                    document.getElementById("articles").style.opacity = 1;
-                }
-            });
-        }
-
-        // Bouge les menus au resize
-        function initResize() {
-            window.addEventListener('resize', function(){
-                moveMenu();
-                if(logged) {
-                    moveSettings();
-                }
-                if(parseInt(window.getComputedStyle(document.body).width) <= 600) {
-                    rotate = true;
-                } else {
-                    rotate = false;
-                }
-            });
-            moveMenu();
-            if(logged) {
-                moveSettings();
-            }
-            if(parseInt(window.getComputedStyle(document.body).width) <= 600) {
-                rotate = true;
+        function toggleMenu() {
+            if(menu_shown) {
+                menu.style.left = "-320px";
+                burger.style.transform = "rotate(0)";
             } else {
-                rotate = false;
+                menu.style.left = 0;
+                burger.style.transform = "rotate(90deg)";
+            }
+            menu_shown = !menu_shown;
+        }
+            
+        // Toggle le profil
+        function toggleProfile() {
+            if(profile_shown) {
+                profile.style.right = "-320px";
+            } else {
+                profile.style.right = 0;
+            }
+            profile_shown = !profile_shown;
+        }
+            
+        document.body.onload = function() {
+            menu = document.getElementById("menu");
+            menu_shown = false;
+            burger = document.getElementById("hamburger");
+            if(logged) {
+                profile = document.getElementById("profile");
+                profile_shown = false;
             }
         }
             
-        function displayArticle() {
-            console.log("test");
-        }
-
-        // Initialisation
-        window.onload = function() {
-            initMenu();
-            if(logged) {
-                initSettings();
-            }
-            initResize();
-        }
         </script>
     </body>
 </html>
